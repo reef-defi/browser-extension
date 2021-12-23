cfe05d1a6b2a6a49c08eaa97b513cc363915b9be
Samo <samo.slag@gmail.com>
2021-12-23 10:59:00 +0100

    Restyle header

--- packages/reef/extension-ui/components/FooterComponent.tsx
+++ packages/reef/extension-ui/components/FooterComponent.tsx
@@ -1,11 +1,11 @@
-import React, {useCallback, useContext} from "react";
-import {ActionContext} from "@reef-defi/extension-ui/components";
-import styled from "styled-components";
+import { ActionContext } from '@reef-defi/extension-ui/components';
+import React, { useCallback, useContext } from 'react';
+import styled from 'styled-components';
 
 interface FooterComp {
 }
 
-function FooterComp(): React.ReactElement<FooterComp> {
+function FooterComp (): React.ReactElement<FooterComp> {
   const onAction = useContext(ActionContext);
   // const selectedAccount = useObservableState(appState.selectedSigner$)
   const openRoute = useCallback(
@@ -14,35 +14,41 @@ function FooterComp(): React.ReactElement<FooterComp> {
   );
 
   return (<div className='nav-content nav-footer navigation d-flex d-flex-space-between'>
-    <nav className="d-flex justify-content-end d-flex-vert-center">
-      <ul className="navigation_menu-items ">
-        <li className="navigation_menu-items_menu-item _text-color-dark-accent">
-          <a href='#'
-             className="navigation_menu-items_menu-item_link"
-             onClick={(ev) => {
-               ev.stopPropagation();
-               ev.preventDefault();
-               openRoute('/')
-             }}>Dashboard</a></li>
-        <li className="navigation_menu-items_menu-item">
-          <a className="navigation_menu-items_menu-item_link"
-             href='#'
-             onClick={(ev) => {
-               ev.stopPropagation();
-               ev.preventDefault();
-               openRoute('/transfer')
-             }}>Send</a></li>
-        <li className="navigation_menu-items_menu-item">
-          <a className="navigation_menu-items_menu-item_link"
-             href='#'
-             onClick={(ev) => {
-               ev.stopPropagation();
-               ev.preventDefault();
-               openRoute('/swap')
-             }}>Swap</a></li>
+    <nav className='d-flex justify-content-end d-flex-vert-center'>
+      <ul className='navigation_menu-items '>
+        <li className='navigation_menu-items_menu-item _text-color-dark-accent'>
+          <a
+            className='navigation_menu-items_menu-item_link'
+            href='#'
+            onClick={(ev) => {
+              ev.stopPropagation();
+              ev.preventDefault();
+              openRoute('/');
+            }}
+          >Dashboard</a></li>
+        <li className='navigation_menu-items_menu-item'>
+          <a
+            className='navigation_menu-items_menu-item_link'
+            href='#'
+            onClick={(ev) => {
+              ev.stopPropagation();
+              ev.preventDefault();
+              openRoute('/transfer');
+            }}
+          >Send</a></li>
+        <li className='navigation_menu-items_menu-item'>
+          <a
+            className='navigation_menu-items_menu-item_link'
+            href='#'
+            onClick={(ev) => {
+              ev.stopPropagation();
+              ev.preventDefault();
+              openRoute('/swap');
+            }}
+          >Swap</a></li>
       </ul>
     </nav>
-  </div>)
+  </div>);
 }
 
 export const FooterComponent = styled(FooterComp)`
