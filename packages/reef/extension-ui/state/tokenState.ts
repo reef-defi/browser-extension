cfd50c67ed0eac650cf46721bcda2371a0814fc1
Matjaz Hirsman <matjazonline@gmail.com>
2021-12-26 16:58:52 +0100

    update reef in tokens on native tx

--- packages/reef/extension-ui/state/tokenState.ts
+++ packages/reef/extension-ui/state/tokenState.ts
@@ -1,6 +1,6 @@
-import {combineLatest, map, Observable, of, shareReplay, startWith, Subject, switchMap, timer} from "rxjs";
-import {filter} from 'rxjs/operators';
-import {api, Network, Pool, ReefSigner, rpc, Token} from "@reef-defi/react-lib";
+import {combineLatest, map, mergeScan, Observable, of, shareReplay, startWith, Subject, switchMap, timer} from "rxjs";
+import {filter, tap} from 'rxjs/operators';
+import {api, Network, Pool, ReefSigner, reefTokenWithAmount, rpc, Token} from "@reef-defi/react-lib";
 import {combineTokensDistinct, toTokensWithPrice} from "./util";
 import {
   selectedNetwork$,
@@ -8,6 +8,7 @@ import {
 } from "./appState";
 import {selectedSigner$} from "./accountState";
 import {getAddressUpdateActionTypes, UpdateDataCtx, UpdateDataType} from "./updateCtxUtil";
+import {BigNumber} from "ethers";
 
 const validatedTokens = require('../validated-tokens-mainnet.json');
 
@@ -20,19 +21,34 @@ export const reefPrice$ = timer(0, 60000).pipe(
 export const validatedTokens$ = of(validatedTokens.tokens as Token[]);
 export const reloadSignerTokens$ = new Subject<void>();
 
+function updateReefBalance(tokens: Token[], balance: BigNumber): Promise<Token[]> {
+  const reefTkn = tokens.find(t => t.address === reefTokenWithAmount().address);
+  if (reefTkn) {
+    reefTkn.balance = balance;
+  }
+  return Promise.resolve([...tokens]);
+}
+
 export const selectedSignerTokenBalances$ = combineLatest([selectedSignerUpdateCtx$, selectedNetwork$, reloadSignerTokens$.pipe(startWith(null))]).pipe(
-  switchMap(([signerCtx, network, _]: [UpdateDataCtx<ReefSigner>, Network, any]) => {
+  mergeScan((state: { tokens: Token[], stopEmit?: boolean }, [signerCtx, network, _]: [UpdateDataCtx<ReefSigner>, Network, any]) => {
     if (!signerCtx.data) {
-      return Promise.resolve([]);
+      return Promise.resolve({tokens: [], stopEmit: false});
     }
     let isTokenUpdate = getAddressUpdateActionTypes(signerCtx.data.address, signerCtx.updateActions).indexOf(UpdateDataType.ACCOUNT_TOKENS) >= 0;
-    if (!isTokenUpdate) {
-      return Promise.resolve(undefined);
+    if (isTokenUpdate) {
+      return api.loadSignerTokens(signerCtx.data, network).then(tokens => ({tokens, stopEmit: false})
+      );
     }
-    return api.loadSignerTokens(signerCtx.data, network);
-  }),
+    let isReefUpdate = getAddressUpdateActionTypes(signerCtx.data.address, signerCtx.updateActions).indexOf(UpdateDataType.ACCOUNT_NATIVE_BALANCE) >= 0;
+    if (isReefUpdate) {
+      return updateReefBalance(state.tokens, signerCtx.data.balance).then(tokens => ({tokens, stopEmit: false}));
+    }
+    return Promise.resolve({tokens: state.tokens, stopEmit: true});
+
+  }, {tokens: [], stopEmit: false}),
 // @ts-ignore
-  filter((v: Token[] | undefined) => v !== undefined),
+  filter((v: { tokens: Token[], stopEmit?: boolean }) => !v.stopEmit),
+  map(state => state.tokens),
   shareReplay(1)
 ) as Observable<Token[]>;
 
