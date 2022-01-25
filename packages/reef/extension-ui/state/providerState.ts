353a312f3a3e8f8b0fb11ecfeec177fdf7f80c2c
Matjaz Hirsman <matjazonline@gmail.com>
2022-01-25 09:54:54 +0100

    update network

--- packages/reef/extension-ui/state/providerState.ts
+++ packages/reef/extension-ui/state/providerState.ts
@@ -1,7 +1,15 @@
-import {BehaviorSubject, ReplaySubject} from "rxjs";
-import {Provider} from "@reef-defi/evm-provider";
-import {Network, Networks, utils} from "@reef-defi/react-lib";
+import {BehaviorSubject, ReplaySubject} from 'rxjs';
+import {Provider} from '@reef-defi/evm-provider';
+import {Network, Networks, utils} from '@reef-defi/react-lib';
 
-export const provider$ = new ReplaySubject<Provider>(1);
-export const networks$ = new BehaviorSubject<Networks>(utils.availableReefNetworks);
-export const selectedNetwork$ = new BehaviorSubject<Network>(networks$.value.mainnet);
+export const providerSubj = new ReplaySubject<Provider>(1);
+export const networksSubj = new BehaviorSubject<Networks>(utils.availableReefNetworks);
+export const selectedNetworkSubj = new BehaviorSubject<Network>(utils.availableReefNetworks.mainnet);
+/*export const selectedNetworkSubj = new BehaviorSubject<Network>({
+  name: 'testnet',
+  rpcUrl: 'ws://localhost:9944',
+  reefscanUrl: 'http://localhost:8000',
+  factoryAddress: '0xcA36bA38f2776184242d3652b17bA4A77842707e',
+  routerAddress: '0x0A2906130B1EcBffbE1Edb63D5417002956dFd41',
+});*/
+selectedNetworkSubj.subscribe((network) => console.log('NETWORK=', network.rpcUrl));
