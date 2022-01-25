cc427ea609c88d249d6fe4503cf719c3178fcfc9
Matjaz Hirsman <matjazonline@gmail.com>
2022-01-25 09:49:44 +0100

    update lib

--- packages/reef/extension-ui/state/tokenState.ts
+++ packages/reef/extension-ui/state/tokenState.ts
@@ -115,7 +115,6 @@ export const allAvailableSignerTokens$ = combineLatest([selectedSignerTokenBalan
 // TODO when network changes signer changes as well? this could make 2 requests unnecessary - check
 export const pools$: Observable<Pool[]> = combineLatest([allAvailableSignerTokens$, selectedNetworkSubj, selectedSigner$]).pipe(
   switchMap(([tkns, network, signer]) => {
-    console.log('LOAD pools=', signer, tkns);
     return signer ? rpc.loadPools(tkns, signer.signer, network.factoryAddress) : [];
   }),
   shareReplay(1),
