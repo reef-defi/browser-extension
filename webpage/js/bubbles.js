508a756dbb9b9bcffd9687dc19fb811f4edcfad5
Samo Šlag <samo.slag@gmail.com>
2022-02-01 21:02:23 +0100

    Create landing page

---
+++ webpage/js/bubbles.js
@@ -0,0 +1,23 @@
+(() => {
+  const header = document.getElementsByClassName("header")[0]
+  if (!header) return
+
+  const random = (min, max) => {
+    return Math.random() * (max - min) + min
+  }
+
+  for (let i = 1; i < 20; i++) {
+    const bubble = document.createElement("div")
+    bubble.setAttribute("class", "bubbles__bubble")
+
+    const style = `
+      animation-delay: ${i * 0.5 - 0.1}s;
+      left: ${random(25, 75)}%;
+      transform: scale(${random(0.75, 1.25)});
+      opacity: ${random(0.5, 1)};
+    `
+    bubble.setAttribute("style", style)
+
+    header.appendChild(bubble)
+  }
+})()
