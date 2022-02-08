1b180415da84a611ff9f0eb0810fe25035515f13
Matjaz Hirsman <matjazonline@gmail.com>
2022-02-08 08:46:19 +0100

    use environment for chain info

--- packages/reef/extension-ui/state/providerState.ts
+++ packages/reef/extension-ui/state/providerState.ts
@@ -1,15 +1,9 @@
 import {BehaviorSubject, ReplaySubject} from 'rxjs';
 import {Provider} from '@reef-defi/evm-provider';
 import {Network, Networks, utils} from '@reef-defi/react-lib';
+import {currentNetwork} from "./environment";
 
 export const providerSubj = new ReplaySubject<Provider>(1);
 export const networksSubj = new BehaviorSubject<Networks>(utils.availableReefNetworks);
-export const selectedNetworkSubj = new BehaviorSubject<Network>(utils.availableReefNetworks.mainnet);
-/*export const selectedNetworkSubj = new BehaviorSubject<Network>({
-  name: 'testnet',
-  rpcUrl: 'ws://localhost:9944',
-  reefscanUrl: 'http://localhost:8000',
-  factoryAddress: '0xcA36bA38f2776184242d3652b17bA4A77842707e',
-  routerAddress: '0x0A2906130B1EcBffbE1Edb63D5417002956dFd41',
-});*/
+export const selectedNetworkSubj = new BehaviorSubject<Network>(currentNetwork);
 selectedNetworkSubj.subscribe((network) => console.log('NETWORK=', network.rpcUrl));
