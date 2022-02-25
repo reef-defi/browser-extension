86e1309163a7ff5cb4c4406cf59275b5bda04e1f
Matjaz Hirsman <matjazonline@gmail.com>
2022-02-25 17:18:14 +0100

    cleanup

--- packages/reef/extension-ui/components/Bind.tsx
+++ packages/reef/extension-ui/components/Bind.tsx
@@ -3,38 +3,6 @@ import React, {useEffect, useState} from 'react';
 import {EvmBindComponent} from './EvmBindComponent';
 import {SigningOrChildren} from './SigningOrChildren';
 
-/*const onTxUpdate = (state: utils.TxStatusUpdate) => {
-  let updateActions: UpdateAction[] = [];
-
-  if (state.componentTxType === EvmBindComponentTxType.BIND) {
-    // bind
-    if (state.addresses && state.addresses.length) {
-      state.addresses.forEach((address) => {
-        updateActions.push({
-          type: UpdateDataType.ACCOUNT_EVM_BINDING,
-          address
-        } as UpdateAction);
-        updateActions.push({
-          type: UpdateDataType.ACCOUNT_NATIVE_BALANCE,
-          address
-        } as UpdateAction);
-      });
-    } else {
-      updateActions = [{ type: UpdateDataType.ACCOUNT_EVM_BINDING }, { type: UpdateDataType.ACCOUNT_NATIVE_BALANCE }];
-    }
-  } else {
-    // transaction
-    updateActions = state.addresses && state.addresses.length
-      ? state.addresses.map((address) => ({
-        type: UpdateDataType.ACCOUNT_NATIVE_BALANCE,
-        address
-      } as UpdateAction))
-      : [{ type: UpdateDataType.ACCOUNT_NATIVE_BALANCE }];
-  }
-
-  onTxUpdateReloadSignerBalances(state, updateActions);
-};*/
-
 export const Bind = (): JSX.Element => {
   const accounts: ReefSigner[] | undefined = hooks.useObservableState(appState.signers$);
   const selectedSigner: ReefSigner | undefined = hooks.useObservableState(appState.selectedSigner$);
