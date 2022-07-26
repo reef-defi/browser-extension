f71256b11c7e35d9709de2e14abc86d0e993833a
Luka Ločniškar <luka@hudlajf.com>
2022-07-26 10:49:08 +0200

    fix: Running local development watch (#39)

--- packages/extension/webpack.watch.cjs
+++ packages/extension/webpack.watch.cjs
@@ -2,10 +2,29 @@
 // SPDX-License-Identifier: Apache-2.0
 
 const createConfig = require('./webpack.shared.cjs');
+const path = require('path');
 
 module.exports = createConfig({
   background: './src/background.ts',
   content: './src/content.ts',
   extension: './src/extension.ts',
   page: './src/page.ts'
+},
+{
+  '@polkadot/wasm-crypto-wasm/data.js': require.resolve('@polkadot/wasm-crypto-wasm/empty')
+},
+{
+  splitChunks: {
+    chunks: 'all',
+    maxSize: 4000000,
+    minSize: 1000000
+  }
+},
+{
+  chunkFilename: '[name].js',
+  filename: (pathData) => {
+    return `/extension-js/${pathData.chunk.id}.js`;
+  },
+  globalObject: '(typeof self !== \'undefined\' ? self : this)',
+  path: path.join(__dirname, 'build')
 });
