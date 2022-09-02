4ac222a40c413f05d23dc30158edb44e0e4d87f5
Matjaz Hirsman <matjazonline@gmail.com>
2022-09-02 14:07:42 +0200

    use extension account metadata to select account

--- packages/reef/extension-base/background/handlers/ReefExtension.ts
+++ packages/reef/extension-base/background/handlers/ReefExtension.ts
@@ -2,8 +2,7 @@ import Extension from "@reef-defi/extension-base/background/handlers/Extension";
 import State from "@reef-defi/extension-base/background/handlers/State";
 import {
     MessageTypes,
-    RequestAccountForget, RequestAccountSelect,
-    RequestAccountShow,
+    RequestAccountSelect,
     RequestTypes,
     ResponseType
 } from "@reef-defi/extension-base/background/types";
@@ -19,7 +18,6 @@ export default class ReefExtension extends Extension {
     }
 
     override async handle<TMessageType extends MessageTypes> (id: string, type: TMessageType, request: RequestTypes[TMessageType], port: chrome.runtime.Port): Promise<ResponseType<TMessageType>> {
-        console.log("REEF HANDLER=", type);
         switch (type) {
             case "pri(accounts.select)":
                 return this.accountsSelect(request as RequestAccountSelect);
@@ -28,8 +26,10 @@ export default class ReefExtension extends Extension {
     }
 
     private accountsSelect ({ address }: RequestAccountSelect): boolean {
-        console.log("accountsSelect=",address);
         const currSelected = keyring.getPairs().find(p => p.meta.isSelected === true);
+        const newSelectPair = keyring.getPair(address);
+        assert(newSelectPair, 'Unable to find pair');
+        // assert(!newSelectPair.meta.isHidden, 'Can not select hidden account');
         if(currSelected) {
             if (currSelected.address === address) {
                 return true;
@@ -37,35 +37,9 @@ export default class ReefExtension extends Extension {
             keyring.saveAccountMeta(currSelected, {...currSelected.meta, isSelected: false});
         }
 
-        const pair = keyring.getPair(address);
-        assert(pair, 'Unable to find pair');
-
-        keyring.saveAccountMeta(pair, { ...pair.meta, isSelected:true });
+        keyring.saveAccountMeta(newSelectPair, { ...newSelectPair.meta, isSelected:true });
 
         accountsObservable.subject.next(accountsObservable.subject.getValue());
-        return true;
-    }
-
-    override accountsForget ({ address }: RequestAccountForget): boolean {
-        const currSelected = keyring.getPairs().find(p => p.meta.isSelected === true);
-        if(currSelected?.address===address){
-            return false;
-        }
-
-        return super.accountsForget({address});
-    }
-
-    override accountsShow ({ address, isShowing }: RequestAccountShow): boolean {
-        const pair = keyring.getPair(address);
-        const currSelected = keyring.getPairs().find(p => p.meta.isSelected === true);
-        if(currSelected?.address===address){
-            return false;
-        }
-
-        assert(pair, 'Unable to find pair');
-
-        keyring.saveAccountMeta(pair, { ...pair.meta, isHidden: !isShowing });
-
         return true;
     }
 
