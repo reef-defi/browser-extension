cf6c98ebabc9ab1d654277b2f99cfbc00ed0b09f
Matjaz Hirsman <matjazonline@gmail.com>
2022-09-15 14:44:53 +0200

    bump version to 1.0.12

--- bumpVersion.js
+++ bumpVersion.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const NEW_VERSION = '1.0.11';
+const NEW_VERSION = '1.0.12';
 
 function getPackageJson (filePath) {
   return { json: JSON.parse(fs.readFileSync(filePath, 'utf8')), filePath: filePath };
