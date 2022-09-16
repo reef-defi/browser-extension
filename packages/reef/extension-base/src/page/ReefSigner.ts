7a5e123d7211cc780449c05495d91419b705c51e
Matjaz Hirsman <matjazonline@gmail.com>
2022-09-16 18:26:28 +0200

    inject signer interface

--- packages/reef/extension-base/src/page/ReefSigner.ts
+++ packages/reef/extension-base/src/page/ReefSigner.ts
@@ -1,21 +1,21 @@
 import { Provider, Signer as ReefEVMSigner } from '@reef-defi/evm-provider';
 import Accounts from '@reef-defi/extension-base/page/Accounts';
 import Signer from '@reef-defi/extension-base/page/Signer';
-import { InjectedAccount, Unsubcall } from '@reef-defi/extension-inject/types';
+import { InjectedAccount, ReefInjectedSigner, Unsubcall } from '@reef-defi/extension-inject/types';
 
-import { ReefInjectedProvider } from './ReefInjectedProvider';
+import { ReefProvider } from './ReefProvider';
 
-export class ReefInjectedSigner {
+export class ReefSigner implements ReefInjectedSigner {
   private accounts: Accounts;
   private extSigner: Signer;
-  private network: ReefInjectedProvider;
+  private injectedProvider: ReefProvider;
   private selectedProvider: Provider | undefined;
   private selectedSignerAccount: InjectedAccount | undefined;
 
-  constructor (accounts: Accounts, extSigner: Signer, network: ReefInjectedProvider) {
+  constructor (accounts: Accounts, extSigner: Signer, injectedProvider: ReefProvider) {
     this.accounts = accounts;
     this.extSigner = extSigner;
-    this.network = network;
+    this.injectedProvider = injectedProvider;
   }
 
   public subscribeSelectedAccount (cb: (accounts: InjectedAccount | undefined) => unknown): Unsubcall {
@@ -25,7 +25,7 @@ export class ReefInjectedSigner {
   }
 
   public subscribeSelectedAccountSigner (cb: (reefEVMSigner: ReefEVMSigner | undefined) => unknown): Unsubcall {
-    const unsubProvFn = this.network.subscribeSelectedNetworkProvider((provider) => {
+    const unsubProvFn = this.injectedProvider.subscribeSelectedNetworkProvider((provider) => {
       this.selectedProvider = provider;
       this.onSelectedSignerParamUpdate(cb);
     });
